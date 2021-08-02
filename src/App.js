c99ab38f91c646dc3d196b8009ceffdbce2e1835
Ubuntu <ubuntu@ip-192-168-1-151.ap-southeast-1.compute.internal>
2021-08-02 17:13:20 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js(under > amplifyapp > src)</code> and save to reload.
+          I <code>src/am Sai Pavan</code> Puligadda
         </p>
         <a
           className="App-link"
